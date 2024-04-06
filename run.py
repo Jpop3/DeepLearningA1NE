dcd8fe5440022e77108346e64238991c46e79e14
Joshua Pople <jpop4511@uni.sydney.edu.au>
2024-04-06 12:13:37 +1100

    cleaning up run.py

--- run.py
+++ run.py
@@ -3,32 +3,14 @@ import pandas as pd
 from MLP import *
 
 input = np.load('Assignment1-Dataset/train_data.npy')
-
-#print(input.shape) #50000 vectors of size 128.
-
 labels = np.load('Assignment1-Dataset/train_label.npy')
-#print(labels.shape) #1 label for each vector of size 128. - Need to convert this to a vector of [0,0,0,1,0, ...]
+
 
 #https://chat.openai.com/c/83234cc9-7d6b-43eb-8d9e-c87b68e5e125
 def class_to_one_hot(class_label, num_classes):
     one_hot = np.zeros(num_classes)
     one_hot[class_label] = 1.
     return one_hot
-
-def bin_class():
-    #----------------------------------------------------------------------------------------
-    #BASIC binary classifier
-    # class_1 = np.hstack([np.random.normal( 1, 1, size=(25, 2)),  np.ones(shape=(25, 1))])
-    # class_2 = np.hstack([np.random.normal(-1, 1, size=(25, 2)), -np.ones(shape=(25, 1))])
-    # dataset = np.vstack([class_1, class_2])
-
-    # nn = MLP([2,3,1], [None,'tanh','tanh'])
-    # input_data = dataset[:,0:2]
-    # output_data = dataset[:,2]
-    # MSE = nn.fit(input_data, output_data, learning_rate=0.001, epochs=500)
-    # print('loss:%f'%MSE[-1])
-    #----------------------------------------------------------------------------------------
-    pass
 
 TRAINING_SIZE = 40000
 # Set numpy seed for reproducibility
@@ -66,19 +48,11 @@ labels_val = np.array(labels_val)
 
 ##### Model #####
 
-#Multi class classifer - fitting the neural network
-# nn = MLP([128, 64, 32, 10], [None, 'ReLU', 'ReLU', 'softmax'], use_batch_norm=True) #can adjust hidden layers and activation functions
-
-# Can pass None in for validation data if we dont want to use it
-# CEL = nn.fit(input_training, labels_training, input_val, labels_val, learning_rate=0.001, epochs=100, batch_size=2) #can change parameters
-# CEL = nn.fit(input_training, labels_training, input_val, labels_val, learning_rate=0.001, epochs=5, batch_size=1) #can change parameters
-# print('Final training loss:%f'%CEL[-1])
-
 nn = MLP(SETUP['hidden_layers'], SETUP['activations'], SETUP['bn'])
 CEL = nn.fit(input_training, labels_training, input_val, labels_val, learning_rate=SETUP['lr'], epochs=SETUP['epochs'], batch_size=SETUP['batch_size'])
 
 
-    ###### Results ######
+###### Results ######
 PRINT_RESULTS = True
 
 ### Training performance ###
@@ -131,14 +105,6 @@ if PRINT_RESULTS:
 
 print(f'Train accuracy: {correct_train_count/TRAINING_SIZE} (count {correct_train_count} of {TRAINING_SIZE})') # train accuracy
 print(f'Test accuracy: {correct_test_count/10000} (count {correct_test_count} of {10000})') # test accuracy
-
-#Loss jumped back up?? after hitting 1.30 to 1.61 and more - this was because wasnt shuffling minibatches
-#Internet said make the learning rate smaller. but no doesnt get to optimal value fast enough?
-#https://www.analyticsvidhya.com/blog/2021/06/the-challenge-of-vanishing-exploding-gradients-in-deep-neural-networks/
-#Wasnt a problem with 1 hidden layer with batch size 2
-# batch = 4, HL = 1, epochs = 100, test acc = 49%, loss = 1.31
-# batch = 4, HL = 2, epochs = 100, test acc = 49%, loss = 1.29
-# batch = 2, HL = 2, epochs = 100, test acc = 52%, loss = 1.10
 
 # Seed 0
 # 'input_size': 128, 'hidden_layers': [128, 64, 32, 10], 'activations': [None, 'ReLU', 'ReLU', 'softmax'], 'bn': True,  'lr': 0.001, 'epochs': 5, 'batch_size': 2
